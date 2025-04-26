5339b1db1813a6c5c3bbfc6ce9a598f51508bbfa
jaimeg12 <James.T.Graft.23@Dartmouth.edu>
2025-04-26 19:45:34 -0400

    rm nodemodules

--- frontend/src/Components/Feedback.jsx
+++ frontend/src/Components/Feedback.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import '../styles/feedback.css'
 
 function Feedback({frames, feedback}) {
   return (<>
     <div>
       {frames && frames.length > 0 && frames.map((v, i) => <>
         <p>
-          {i}: 
+          {i + 1}: 
         </p>
         
         <img
           src={frames[i] ?? "Loading..."}
+          className='feedback-image'
         />
 
         <p>
