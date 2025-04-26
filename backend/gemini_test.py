e1b08c34f938279ec81c9a5bcce4e932398ffd1f
Owen Chen <owen.c.learning@gmail.com>
2025-04-26 12:51:30 -0400

    added backend folder

---
+++ backend/gemini_test.py
@@ -0,0 +1,12 @@
+from google import genai
+
+client = genai.Client(api_key="AIzaSyAxtl5MUd4ar9njJDck7IRU1FAjsNkHpT0")
+
+my_file = client.files.upload(file="images/08b374fa-da4e-455c-bbc7-0f700148a2b7.png")
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents=[my_file, "You are provided with a single keyframe image captured from a sign language learner’s motion. Evaluate this keyframe according to each of the categories in the following guide, then provide feedback to the user:\nHandshape: The handshape is a B-handshape (also sometimes called a Flat Hand). The fingers are extended and held together, and the thumb is held alongside the palm/index finger.\nLocation: The sign is made at the center of the chest.\nPalm Orientation: The palm is oriented inward, towards the signer's body.\nFacial Expression/Non-Manual Signals (NMS): The signer has a pleasant, smiling facial expression and is looking forward. This generally conveys a positive or neutral emotion."],
+)
+
+print(response.text)
